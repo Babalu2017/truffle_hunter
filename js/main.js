efc58e3a8e686e0c5ac0afdb389bd338dcfbdae5
Francesco Miranda <francescomiranda@Francescos-MacBook-Pro.local>
2022-03-02 22:56:49 -0800

    added scroll spy feature to all markers

--- js/main.js
+++ js/main.js
@@ -37,60 +37,60 @@ function initMap() {
         },
         {
             coords: {lat:51.529972,lng:-0.127676},
-            content: `<h6>The British Library</h6>`
+            content: `<a href="#britishLibrary" >The British Library</a>`
         },
         {
             coords: {lat:51.59177885511457,lng:-0.1429820018890784},
-            content: `<h6>Sable D'Or</h6>`
+            content: `<a href="#sable">Sable D'Or</a>`
         },
         {
             coords: {lat:51.521376524551144,lng:-0.13473271550019372},
-            content: `<h6>The Ambrose Cafe, Heal's</h6>`
+            content: `<a href="#ambrose">The Ambrose Cafe, Heal's</a>`
         },
         {
             coords: {lat:51.58880969653916,lng:-0.040074844332947786},
-            content: `<h6>Creative Works Space</h6>`
+            content: `<a href="#workSpace">Creative Works Space</a>`
         },
         {
             coords: {lat:51.54830676155435,lng:-0.10669608666431893},
-            content: `<h6>Vagabond N7</h6>`
+            content: `<a href="#vagabond">Vagabond N7</a>`
         },
         {
             coords: {lat:51.42270171481481,lng:-0.20776925967845764},
-            content: `<h6>Wimbletech Wimbledon</h6>`
+            content: `<a href="#wimbletech">Wimbletech Wimbledon</a>`
         },
         {
             coords: {lat:51.47422354579121,lng:0.07725887501669498},
-            content: `<h6>The Peckham Pelican</h6>`
+            content: `<a href="#pelican">The Peckham Pelican</a>`
         },
         {
             coords: {lat:51.501963151225816,lng:-0.09229364249091189},
-            content: `<h6>John Harvard Library</h6>`
+            content: `<a href="#harvard">John Harvard Library</a>`
         },
         {
             coords: {lat:51.52026554939279,lng:-0.07033724433521935},
-            content: `<h6>Second Home, Hanbury Street</h6>`
+            content: `<a href="#secondHome">Second Home, Hanbury Street</a>`
         },
         {
             coords: {lat:51.50770658988891,lng:0.019626399839277223},
-            content: `<h6>The Good Hotel, Royal Victoria Dock</h6>`
+            content: `<a href="#goodHotel" >The Good Hotel, Royal Victoria Dock</a>`
         },
         {
             coords: {lat:51.49802508791992,lng:-0.014858115501003208},
-            content: `<h6>The Collective Canary Wharf</h6>`
+            content: `<a href="#collectiveCanaryWharf">The Collective Canary Wharf</a>`
         },
         {
             coords: {lat:51.529046170320484,lng:-0.250359086664928},
-            content: `<h6>The Collective Old Oak, North Acton</h6>`
+            content: `<a href="#collectiveOldOak">The Collective Old Oak, North Acton</a>`
         },
         ,{
             coords: {lat:51.49238691926185,lng:-0.1588864866661796},
-            content: `<h6>Peter Jones (top floor restaurant), Sloane Square
-            </h6>`
+            content: `<a href="#peterJones">Peter Jones (top floor restaurant), Sloane Square
+            </a>`
         },
         {
             coords: {lat:51.49401987208651,lng:-0.15866324433611761},
-            content: `<h6>Granger and Co, Chelsea</h6>`
+            content: `<a href="#granger">Granger and Co, Chelsea</a>`
         }
 
     ]
