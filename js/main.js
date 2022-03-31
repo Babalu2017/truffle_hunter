8b526fa7a69c505eb8b0a7f3cc36347efe364821
Francesco Miranda <francescomiranda@Francescos-MacBook-Pro.local>
2022-03-31 02:05:58 +0100

    fixed bug for background img on ios mobile

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-// var _isOS = navigator.userAgent.match(/(iPod|iPhone|iPad)/);
+var _isOS = navigator.userAgent.match(/(iPod|iPhone|iPad)/);
 
-// if (_isOS) {
-//   $('body').addClass('is-os');
-// }
+if (_isOS) {
+  $('body').addClass('is-os');
+}
