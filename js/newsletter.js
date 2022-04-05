9a09aa45b8b3239108cb3017b678b252ce5ad8de
Francesco Miranda <francescomiranda@Francescos-MacBook-Pro.local>
2022-04-05 23:12:45 +0100

    added code for newsletter emailjs

---
+++ js/newsletter.js
@@ -0,0 +1,41 @@
+const form2 = document.getElementById('my_form2');
+
+    form2.addEventListener('submit', function (event) {
+    //prevent the reload of the page. here i prevent the event.
+    event.preventDefault();
+
+
+    const fullName = document.getElementById('fullName').value;
+    const email = document.getElementById('email').value;
+    
+
+    //Sending the email with the name and email
+    emailjs.sendForm("service_lhay6fq", "Francesco", '#my_form2', 'b166ofxC1GD8wWnmt', {
+        "from_name": fullName,
+        "from_email": email
+
+    })
+        .then(
+            function (response) {
+                const statusMsg = "Awesome! We'll get you updated!"
+                console.log("SUCCESS", response.status);
+                const msgUser = document.getElementById('messageUser');
+                msgUser.textContent = statusMsg;
+                msgUser.style.color = "green";
+                setTimeout(function(){
+                    if ($('#messageUser').length > 0) {
+                      $('#messageUser').remove();
+                      location.reload()
+                    }
+                  }, 3000);        
+            },
+            function (error) {
+                console.log("FAILED", error);
+
+            }
+
+        );
+        
+    form2.reset();
+
+})
